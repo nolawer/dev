3c9f272569ceb7894f53e42634d730105094677f
nolawer <nolawer@gmail.com>
2019-05-27 08:33:47 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+  <!-- Title -->
+  <title>edm IELTS 문제은행</title>
+
+  <!-- Head -->
+  <?php include './head.html';?>
+  <!-- End Head -->
+
+</head>
+
+<body>
+  <main>
+    <!-- Header -->
+    <?php include './header.html';?>
+    <!-- End Header -->
+
+    <!-- Main Content -->
+    <?php include './main-content.html';?>
+    <!-- Main Content -->
+
+    <!-- Footer -->
+    <?php include './footer.html';?>
+    <!-- Footer -->
+
+</body>
+
+</html>
---
+++ test2.php
@@ -0,0 +1,24 @@
+<?php
+
+  try {
+    $pdo = new PDO('mysql:host=localhost; dbname=ijdb; charset=utf8', 'ijdbuser','nomark08151!');
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'SELECT `joketext` FROM `joke`';
+    $result = $pdo -> query($sql);
+
+    while ($row = $result -> fetch()) {
+      $jokes[] = $row['joketext'];
+    }
+
+
+  } catch (PDOException $e) {
+    $output = '데이터베이스에 접속할 수 없습니다.'.
+    $e -> getMessage() . ',위치:' .
+    $e -> getFile() . '라인:' . $e -> getLine();
+  }
+
+include './template/output.html.php';
+
+
+ ?>
